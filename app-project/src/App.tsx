607169e29b6891dff389798faf59b59497aa99f2
Mathilda Shongwe <mshongwe@retrorabbit.co.za>
2024-04-25 07:10:47 +0200

    table added to dashboard

--- app-project/src/App.tsx
+++ app-project/src/App.tsx
@@ -1,38 +1,39 @@
 import { useState } from 'react'
 import { Layout } from 'antd';
 import './App.css'
+import Sidebar from '../components/SideBar'
 import MenuList from '../components/Menu';
 import React from 'react';
 
  function App() {
 
   return (
+<div className='cntr'>
 
-<div>
-  <h3>Latest Requests</h3>
-  <table>
+
+  <table id="requests">
     <tr>
-      <th>Customer</th>
+      <th>Request Type</th>
       <th>Date</th>
-      <th>Service</th>
+      <th>Number of Requests</th>
       <th>Status</th>
     </tr>
     <tr>
-      <td>Jane Doe</td>
-      <td>14 May 2022</td>
       <td>Towing</td>
+      <td>14 May 2022</td>
+      <td>180</td>
       <td><div className='badge'>Approved</div></td>
     </tr>
     <tr>
-      <td>Jane Doe</td>
+      <td>Flat Tyre</td>
       <td>14 May 2022</td>
-      <td>Towing</td>
+      <td>2</td>
       <td><div className='badge-decline'>Decline</div></td>
     </tr>
     <tr>
-      <td>Jane Doe</td>
+      <td>Jump Start</td>
       <td>14 May 2022</td>
-      <td>Towing</td>
+      <td>5</td>
       <td><div className='badge-pending'>Pending</div></td>
     </tr>
   </table>
